bb96b0520c6681c1b5e75bbc14c16d927bd74576
choisunghwan <dute7570@naver.com>
2023-01-10 15:17:06 +0900

    dancer 파일생성 , main 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.basic.Dancer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요!");
+
+//ctrl + alt +v 를 new dancer에 가져다가 누르면 자동으로 변수 생성
+//      Dancer dancer = new Dancer(crewName "총알탄소년단", myName "춤신춤왕"); ==> 여기서 "총알탄소년단" , "춤신춤왕"만 입력하면 된다.
+        Dancer dancer = new Dancer("총알탄소년단","춤신춤왕");
+
+
+        dancer.dance();
     }
 }
--- src/main/java/org/example/basic/Dancer.java
+++ src/main/java/org/example/basic/Dancer.java
@@ -1,7 +1,59 @@
 package org.example.basic;
 
+
+import static java.lang.Math.*;
+import static java.lang.System.*;
+import static org.example.basic.DanceLevel.*;
+
 public class Dancer {
     
-    private String crewName; //팀의 이름
-    private String myName; // 내 이름
+    private final String crewName; //팀의 이름 . final필수, dancer로 태어나면 불변값.
+    private final String myName; // 내 이름
+
+    private  DanceLevel level = PRO; //춤 수준(전문가. 중수, 하수. 초보자) //필수 불변 값은 아니니까 final 안 붙였음.
+
+    // 스트레칭 했는지
+    private  boolean stretch;
+
+    //유연성 운동 했는지
+    private  boolean flexible;
+
+    //생성자 : 객체가 처음 생성될때 호출되는 메서드
+    public Dancer(String crewName, String myName){
+        this.crewName = crewName;
+        this.myName=myName;
+
+        random(); //import 로 설정시 math.random() 안해도 됨. (alt +enter 누르면 됨)
+        ceil(1.1 * PI);
+        out.println(); //sout 단축키 + import 로 인해 System.을 때도 됨.
+    }
+
+
+    // 스트레칭
+    private void stretch(){
+        out.println(myName+"님이 스트레칭합니다.");
+        this.stretch=true;
+    }
+    //다리찢기
+    private void makeFlexible(){
+        if(!stretch){
+            out.println("스트레칭 안해서 죽었습니다");
+            return;
+        }
+        out.println(myName+"님이 유연성 운동을 합니다.");
+        this.flexible=true;
+    }
+
+    //춤추기
+    public void dance(){
+
+        stretch();
+        makeFlexible();
+
+        if(!flexible){
+            out.println("유연성 운동 안해서 죽었습니다.");
+            return;
+        }
+        out.println(myName+"님이 충을 춥니다");
+    }
 }
